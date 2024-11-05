f703d9a86702293e8236e881a658b772dede2952
e.levin <e.levin@solarlab.ru>
2024-11-05 17:40:01 +0300

    Удалил лишние импорты

--- src/main/java/com/sevsu/intercitydelivery/IntercityDeliveryApplication.java
+++ src/main/java/com/sevsu/intercitydelivery/IntercityDeliveryApplication.java
@@ -4,10 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableFeignClients
